373825975f6734293e32ffbd266e2ef8ca810dea
kisumutalent <homiemusa@gmail.com>
2019-04-27 06:45:05 +0300

    add pagemarkdown

--- project/posts/forms.py
+++ project/posts/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 
 from .models import Post
+from pagedown.widgets import PagedownWidget
 
 class PostForms(forms.ModelForm):
+	content = forms.CharField(widget=PagedownWidget())
+	publish = forms.DateField(widget=forms.SelectDateWidget())
+
 	class Meta:
 		model = Post
 		fields = [
