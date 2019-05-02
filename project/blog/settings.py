af80204c0f922672361e3927dedbf9773adb0c60
kisumutalent <homiemusa@gmail.com>
2019-05-02 11:34:00 +0300

    Add crispy forms

--- project/blog/settings.py
+++ project/blog/settings.py
@@ -38,11 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'markdown_deux',
+    'crispy_forms',
     'pagedown',
     'posts',
 ]
-
-MIDDLEWARE = [
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
+MIDDLEWARE = [  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
