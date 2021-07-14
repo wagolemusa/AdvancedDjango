e77d1c95f27d656d32cd77dcad879a1e6c37ec25
kisumutalent <homiemusa@gmail.com>
2021-07-14 07:45:11 +0300

    Make Changes

--- blog/settings.py
+++ blog/settings.py
@@ -81,11 +81,31 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'xycoders',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'refuge',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
+
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
