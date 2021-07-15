b25431e17cbdcded02f91ce31a6af9dc6ebb06cf
kisumutalent <homiemusa@gmail.com>
2021-07-15 09:11:54 +0300

    edit files

--- blog/settings.py
+++ blog/settings.py
@@ -153,14 +153,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-
-# STATIC_URL = '/static/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     # '/var/www/static/',
-# ]
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 MEDIA_URL  = "/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_evn')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
